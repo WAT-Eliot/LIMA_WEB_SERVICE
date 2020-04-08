87b06f2b9757ffb3e85033bb0c20fbdb114e8e87
Eliot ANDREIS <e.andreis@wat.digital>
2020-04-08 18:08:09 +0200

    DLL PcVue

---
+++ usrmgrDotNetProject/usrmgrDotNetProject/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usrmgrDotNetProject
+{
+    public class Main:IDisposable 
+    {
+        public Main()
+        {
+            SvMgrAPI.StartProject += SvMgrAPI_StartProject;
+        }
+
+        private void SvMgrAPI_StartProject()
+        {
+            SvMgrAPI.LogMessage(SvMgrEnums.LogMessageLevel.Info, "DLL démarrée");
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
