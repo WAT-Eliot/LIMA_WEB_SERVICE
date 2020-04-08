e911636fef894f239f632ffe0c6bb11e7731d2cd
Eliot ANDREIS <e.andreis@wat.digital>
2020-04-08 11:11:10 +0200

    Mise a jour

--- DLL_Lima/ConsoleApplication1/ConsoleApplication1.cpp
+++ DLL_Lima/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,20 +8,30 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Web_Service\n";
 
+	int iVal = 0;
 	
-
 	ns3__PostTapingCollectShuttleMissionStatusRequest MissionStatusResquest = ns3__PostTapingCollectShuttleMissionStatusRequest();
 	MissionStatusResquest.MissionNumber = new int(123);
 	MissionStatusResquest.MissionStatus = new ns3__PostTapingCollectShuttleMissionStatusType(ns3__PostTapingCollectShuttleMissionStatusType__Ok);
 	
-	_ns1__MissionStatus MissionStatus = _ns1__MissionStatus();
-	MissionStatus.request = new ns3__PostTapingCollectShuttleMissionStatusRequest(MissionStatusResquest);
+	_ns1__MissionStatus * MissionStatus = new _ns1__MissionStatus();
+	MissionStatus->request = new ns3__PostTapingCollectShuttleMissionStatusRequest(MissionStatusResquest);
+
+	_ns1__MissionStatusResponse MissionStatusResponse = _ns1__MissionStatusResponse();
 
 	BasicHttpBinding_USCOREIInterfacesPostTapingCollectShuttleToMx01Proxy client(SOAP_XML_INDENT);
 
-	client.MissionStatus = MissionStatus;
+	iVal = client.MissionStatus(MissionStatus, MissionStatusResponse);
+	if (iVal == SOAP_OK) {
+		std::cout << "Mission Status OK\n";
+	}
+	else {
+		std::cout << "Mission Status NOK\n";
+		client.soap_stream_fault(std::cerr);
+	}
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
